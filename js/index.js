81d85651245398e0ebafdf48b4573a69a2b8afb2
yonathan <yonathanterrazas989@gmail.com>
2023-06-13 11:28:28 -0300

    ven

---
+++ js/index.js
@@ -0,0 +1,61 @@
+//comentario de linea
+
+/*
+comentario
+de bloque
+*/
+/*
+//OPERACIONES BASICAS
+let n1 = 3
+let n2 = 5
+let n3 = 6
+
+//SUMA
+
+let resultadoSuma = n1 + n2
+console.log(resultadoSuma)
+
+//RESTA
+/*
+let resultadoResta = n1 - n3
+console.log(resultadoResta)
+//MULTIPLICACION
+let resultadoMult =n2 * n3
+console.log(resultadoMult)
+
+//variables
+//declaracion / nombre / asignacion del valor //
+let nombreUsuario = prompt('Ingresar nombre de ususario')
+let contraseña = prompt('ingresar tu contraseña')
+
+if ((nombreUsuario== 'yonathan') && (contrasenia == '1234')){
+    alert('Bienvenido Yonathan')
+    
+}else{
+    alert('Nombre de usurio y/o contraseña incorrectos')
+}
+*/
+//CONDICIONALES: estructuras sintacticas que sirven para tomar 1 decision a partir de una condicion
+//IF
+// si <condicion> entoces <operacion
+/*
+if (true) {
+    //bloque de codigo
+    console.log ('vas a ver este mensaje')
+}
+*/
+/* let numero = 8
+if (numero ==8 ) {
+    console .log ('Vas a ver este mensaje')
+
+
+}
+*/
+/*
+let color = 'Azul'
+if (color == 'Azul' || 'azul') {   
+console.log('El color es azul')
+ } else{
+    console.log('El color no es azul')
+ }
+ */
